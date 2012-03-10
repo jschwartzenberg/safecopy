a3621f02f9963c13ca450ed0770b098561e1084d
corvuscorax <>
2012-03-10 21:41:11 +0000

    updated self tests to support test of continuing command line feature

--- safecopy/test/test12/test.sh
+++ safecopy/test/test12/test.sh
@@ -10,8 +10,9 @@ function test_current() {
 	cp "test1.dat" "$testsuite_tmpdir/test1.dat" >/dev/null 2>&1
 	
 	testsuite_debug "Test, safecopy must not overwrite data between include blocks"
-	if ! LD_PRELOAD="$preload" $safecopy -M "MARKBAAD" -R 2 -b 1024 -f 1* -I iblocks debug "$testsuite_tmpdir/test1.dat" >"$testsuite_tmpdir/test1.out" 2>&1; then
-		testsuite_error "Run of safecopy failed. Output:"
+	LD_PRELOAD="$preload" $safecopy -M "MARKBAAD" -R 2 -b 1024 -f 1* -I iblocks debug "$testsuite_tmpdir/test1.dat" >"$testsuite_tmpdir/test1.out" 2>&1
+	if [ $? != 1 ]; then
+		testsuite_error "Run of safecopy returned wrong exit code. Output:"
                 testsuite_debug_file "$testsuite_tmpdir/test1.out"
 	fi
 	testsuite_assert_files_identical "test1.dat" "$testsuite_tmpdir/test1.dat"
